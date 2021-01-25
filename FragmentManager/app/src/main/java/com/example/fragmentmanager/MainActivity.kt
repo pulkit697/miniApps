2532cd6be9213e547bb4a1c0223955fbbc72cf7c
pulkit <pulkitgarg697@gmail.com>
2021-01-25 11:28:16 +0530

    dynamic fragments using fragment manager

--- FragmentManager/app/src/main/java/com/example/fragmentmanager/MainActivity.kt
+++ FragmentManager/app/src/main/java/com/example/fragmentmanager/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.fragmentmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.frag_container,FirstFragment())
+            .commitNow()
+
+        btSwitch.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.frag_container,SecondFragment())
+                .commitNow()
+        }
+
     }
 }
