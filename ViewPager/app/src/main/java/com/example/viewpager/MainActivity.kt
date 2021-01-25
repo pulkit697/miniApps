1dc1278276293f1da8a70e147104913c7e72ea70
pulkit <pulkitgarg697@gmail.com>
2021-01-25 13:33:05 +0530

    view Pager depreciated

--- ViewPager/app/src/main/java/com/example/viewpager/MainActivity.kt
+++ ViewPager/app/src/main/java/com/example/viewpager/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.viewpager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+        viewPagerAdapter.apply {
+            add(BlankFragment())
+            add(BlankFragment2())
+            add(BlankFragment3())
+        }
+        vpList.adapter = viewPagerAdapter
     }
 }
---
+++ ViewPager/app/src/main/java/com/example/viewpager/ViewPagerAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.viewpager
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class ViewPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+    val list = arrayListOf<Fragment>()
+
+    fun add(fragment: Fragment){
+        list.add(fragment)
+    }
+
+    override fun getCount(): Int = list.size
+
+    override fun getItem(position: Int): Fragment = list[position]
+}
