e2d4138fc1c27a7a85b5ad7f24984cc386242ccb
pulkit <pulkitgarg697@gmail.com>
2021-02-23 11:59:06 +0530

    color change via accelerometer

--- Sensor/app/src/main/java/com/example/sensor/MainActivity.kt
+++ Sensor/app/src/main/java/com/example/sensor/MainActivity.kt
@@ -11,12 +11,16 @@ import android.util.Log
 import android.widget.Toast
 import androidx.core.content.getSystemService
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.roundToInt
 import kotlin.random.Random
+
+
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var sensorManager: SensorManager
     lateinit var proximitySensor: Sensor
+    lateinit var accSensor:Sensor
     lateinit var sensorEventListener: SensorEventListener
     val colors = arrayOf(Color.RED,Color.BLUE,Color.CYAN,Color.GREEN,Color.MAGENTA,Color.YELLOW)
 
@@ -26,13 +30,28 @@ class MainActivity : AppCompatActivity() {
 
         sensorManager = getSystemService()!!
         proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+        accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
         sensorEventListener = object : SensorEventListener {
             override fun onSensorChanged(event: SensorEvent?) {
                 Log.d("Pulkit","""
-                    Sensor data changed to: ${event!!.values[0]}
+                    -----
+                    ax = ${event!!.values[0]}
+                    ay = ${event.values[1]}
+                    az = ${event.values[2]}
+                    -----
                 """.trimIndent())
-                flBottom.setBackgroundColor(colors[Random.nextInt(6)])
+//                flBottom.setBackgroundColor(colors[Random.nextInt(6)])
+                flBottom.setBackgroundColor(Color.rgb(
+                    getColorAcc(event.values[0]),
+                    getColorAcc(event.values[1]),
+                    getColorAcc(event.values[2])
+                ))
+                flTop.setBackgroundColor(Color.rgb(
+                    getColorAcc(event.values[1]),
+                    getColorAcc(event.values[2]),
+                    getColorAcc(event.values[0])
+                ))
             }
 
             override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
@@ -43,12 +62,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        sensorManager.registerListener(sensorEventListener,proximitySensor,SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(sensorEventListener,accSensor,1000*1000*100)
     }
 
     override fun onPause() {
         super.onPause()
         sensorManager.unregisterListener(sensorEventListener)
     }
+
+    fun getColorAcc(f:Float):Int = (((f+12)/24) *255).roundToInt()
 
 }
