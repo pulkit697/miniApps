88b8f6c4a74ee8efa4eaee70b9b19186d0c45e0c
Pulkit-Windows <pulkitgarg697@gmail.com>
2021-04-12 01:57:39 +0530

    connected to firebase read and wrote string data into realtime database

---
+++ FireBasetutorial/app/src/main/java/com/example/firebasetutorial/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.firebasetutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private val dbReference = FirebaseDatabase.getInstance().reference
+    private val notesList = arrayListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,notesList)
+        listNotes.adapter = adapter
+
+        dbReference.child("note").addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                notesList.add(snapshot.value.toString())
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+    }
+
+    fun saveToDatabase(view: View) {
+        if(etTextValue.text.isNotBlank())
+            dbReference.child("note").push().setValue(etTextValue.text.toString())
+    }
+}
