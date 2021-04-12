a07fadc2eab97ebd286a73ea8a8a36165bf224c6
Pulkit-Windows <pulkitgarg697@gmail.com>
2021-04-12 15:08:25 +0530

    email authentication log in to app via google or email. some error in google log-in + phone log-in not implemented

--- FireBasetutorial/app/src/main/java/com/example/firebasetutorial/MainActivity.kt
+++ FireBasetutorial/app/src/main/java/com/example/firebasetutorial/MainActivity.kt
@@ -1,26 +1,51 @@
 package com.example.firebasetutorial
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.ArrayAdapter
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
     private val dbReference = FirebaseDatabase.getInstance().reference
     private val notesList = arrayListOf<Note>()
+    var firebaseUser:FirebaseUser? = null
+    val RC_SIGN_IN = 123
+    lateinit var adapter: CustomAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val adapter = CustomAdapter(this,R.layout.note_layout,notesList)
+        adapter = CustomAdapter(this, R.layout.note_layout, notesList)
         listNotes.adapter = adapter
 
-        dbReference.child("note").addChildEventListener(object : ChildEventListener {
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+        if(firebaseUser!=null) {
+            //already signed in
+            addListeners()
+        }
+        else{
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+                    .setAvailableProviders(listOf(AuthUI.IdpConfig.GoogleBuilder().build(),
+                            AuthUI.IdpConfig.EmailBuilder().build(),
+                            AuthUI.IdpConfig.PhoneBuilder().build()))
+                    .build(), RC_SIGN_IN)
+        }
+
+    }
+
+    private fun addListeners() {
+        dbReference.child(firebaseUser!!.uid).addChildEventListener(object : ChildEventListener {
             override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
                 snapshot.getValue(Note::class.java)?.let { notesList.add(it) }
                 adapter.notifyDataSetChanged()
@@ -43,10 +68,37 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        btAddToNotes.setOnClickListener {
+            saveToDatabase()
+        }
+
     }
 
-    fun saveToDatabase(view: View) {
+    private fun saveToDatabase() {
         if(etTextValue.text.isNotBlank())
-            dbReference.child("note").push().setValue(Note(etTextValue.text.toString(),etTextValue.text.toString()))
+            dbReference.child(firebaseUser!!.uid).push().setValue(Note(etTextValue.text.toString(), etTextValue.text.toString()))
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                addListeners()
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    return
+                }
+                if (response.error!!.errorCode == ErrorCodes.NO_NETWORK) {
+                    return
+                }
+                Log.d("pulkit", "Sign-in error: ", response.error)
+            }
+        }
     }
 }
